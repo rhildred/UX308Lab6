70a85ae2c5ef3645847e3c758ca4a3907ba2b779
Richard Hildred <rhildred@gmail.com>
2025-04-04 15:46:57 +0000

    eliminate css from contact form microfrontend

--- wp-content/themes/shawarama/functions.php
+++ wp-content/themes/shawarama/functions.php
@@ -13,6 +13,7 @@ function theme_enqueue_styles() {
     wp_enqueue_script('custom_javascript', get_stylesheet_directory_uri() . '/customElements.js');
     wp_enqueue_script_module('custom_module1', "https://rhildred.github.io/chatter/fab.js");
     wp_enqueue_script_module('custom_module2', "https://rhildred.github.io/chatter/chat.js");
+    wp_enqueue_script_module('custom_module3', "https://rhildred.github.io/contact-form/x-form.js");
 }
 //
 // Your code goes below
